dea7a33cae5487dc1ab9df2fab3f92199aaf5673
Juan Camilo Florez <juan.florez@prodigious.com>
2015-07-31 16:30:43 -0500

    cambios relacionados con la instalación y configuración de cassandra
    Former-commit-id: 7c7f786c63d853771874a14ac2414447558902e5

--- start/start_cassandra.command
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /Users/juaflore/Documents/DW/start/dsc-cassandra-2.0.14/bin
-./cassandra
--- start/start_cqlsh.bat
+++
@@ -1,3 +0,0 @@
-set path=c:\users\jflorezr\documents\DW\start\python27
-cd c:\users\jflorezr\documents\DW\start\dsc-cassandra-2.0.14\bin
-cqlsh.bat
